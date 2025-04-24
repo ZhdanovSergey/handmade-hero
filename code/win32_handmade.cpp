7976b2ecd3022970bd29756c19b0de73bed3e04c
Sergei Zhdanov <zhdanovsn@bcs.ru>
2025-04-24 13:19:07 +0700

    Day 001 - Setting Up the Windows Build

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+	MessageBoxA(0, "This is Handmade Hero", "Handmade Hero", MB_OK | MB_ICONINFORMATION);
+	return 0;
+}
